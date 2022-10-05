0b6a810b7aa4b7bd33c97b3561130b0790bdc805
Jean Rey <jeanrey216@gmail.com>
2022-10-04 20:04:42 -0400

    upgrape google and apple logos

--- src/Components/CreateAccount.jsx
+++ src/Components/CreateAccount.jsx
@@ -1,5 +1,6 @@
 import React,{ useState } from "react";
-
+import applelogo from '../images/apple.svg';
+import googlelogo from '../images/google-icon.svg';
 export default function CreateAccount(){
 
     return( 
@@ -8,6 +9,10 @@ export default function CreateAccount(){
     <input type={'name'} className='UserName' placeholder='User Name'></input>
     <input type={'email'} className='User' placeholder='Email'></input>
     <input type={'password'} className='Password' placeholder='Password'></input>
+    <div className='apple'>
+        <img className="applelogo" src={applelogo} alt="apple" />
+        <img className="googlelogo"src={googlelogo} alt="gogle"/>
+    </div>
     </>
     )
        
